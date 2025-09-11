5a9397b8bbbb1f0c3ba39db6958fd18a00a3c729
Markus Harnusek <m.harnusek@students.htl-leonding.ac.at>
2025-09-11 08:36:52 +0200

    Changed server file structure

---
+++ server/Network.cs
@@ -0,0 +1,32 @@
+       
+namespace server
+{
+    /// <summary>
+    /// Handles network related tasks
+    /// </summary>
+    internal class Network
+    {
+        /// <summary>
+        /// Gets the public IPv6 address and falls back to returning the public IPv4 address
+        /// </summary>
+        /// <returns>An IPv6 or IPv4 address</returns>
+        public static async Task<string> GetPublicIpAsync()
+        {
+            using var httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromSeconds(3);
+
+            // Try to obtain the IPv6 address
+            try
+            {
+                return await httpClient.GetStringAsync("https://api64.ipify.org");
+            }
+            catch (Exception e)
+            {
+                Util.Log($"An error occured while obtaining public IPv4 address: {e.Message.ToString()}", LogLevel.Fatal);
+                Environment.Exit(1);
+            }
+
+            return string.Empty;
+        }
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,33 +1,12 @@
-﻿namespace server
+﻿using System.Net.NetworkInformation;
+
+namespace server
 {
     internal class Program
     {
-        /// <summary>
-        /// Gets the public IPv6 address and falls back to returning the public IPv4 address
-        /// </summary>
-        /// <returns></returns>
-        static async Task<string> GetPublicIpAsync()
-        {
-            using var httpClient = new HttpClient();
-            httpClient.Timeout = TimeSpan.FromSeconds(3);
-
-            // Try to obtain the IPv6 address
-            try
-            {
-                return await httpClient.GetStringAsync("https://api64.ipify.org");
-            }
-            catch (Exception e)
-            {
-                Util.Log($"An error occured while obtaining public IPv4 address: {e.Message.ToString()}", LogLevel.Fatal);
-                Environment.Exit(1);
-            }
-
-            return string.Empty;
-        }
-
         static async Task Main(string[] args)
         {
-            string publicIp = await GetPublicIpAsync();
+            string publicIp = await Network.GetPublicIpAsync();
             Util.Log($"Public IP Address: {publicIp}", LogLevel.Ok);
         }
     }
