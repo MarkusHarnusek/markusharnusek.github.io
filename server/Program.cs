cf31201e245bb966079576551b2db0989db88f8e
Markus Harnusek <m.harnusek@students.htl-leonding.ac.at>
2025-09-11 08:27:51 +0200

    Changed IP getter logic

--- server/Program.cs
+++ server/Program.cs
@@ -8,32 +8,21 @@
         /// <returns></returns>
         static async Task<string> GetPublicIpAsync()
         {
-            string response = string.Empty;
             using var httpClient = new HttpClient();
             httpClient.Timeout = TimeSpan.FromSeconds(3);
 
             // Try to obtain the IPv6 address
             try
             {
-                response = await httpClient.GetStringAsync("https://api64.ipify.org");
+                return await httpClient.GetStringAsync("https://api64.ipify.org");
             }
-            catch (Exception IPv6e)
+            catch (Exception e)
             {
-                Util.Log($"Unable to obtain public IPv6 address. Falling back to IPv4: {IPv6e.Message.ToString()}", LogLevel.Warning);
-
-                // Fall back to IPv4 if unable to obtain IPv4
-                try
-                {
-                    response = await httpClient.GetStringAsync("https://api.ipify.org");
-                }
-                catch (Exception IPv4e)
-                {
-                    Util.Log($"An error occured while obtaining public IPv4 address: {IPv4e.Message.ToString()}", LogLevel.Fatal);
-                    Environment.Exit(1);
-                }
+                Util.Log($"An error occured while obtaining public IPv4 address: {e.Message.ToString()}", LogLevel.Fatal);
+                Environment.Exit(1);
             }
 
-            return await httpClient.GetStringAsync("https://api.ipify.org");
+            return string.Empty;
         }
 
         static async Task Main(string[] args)
