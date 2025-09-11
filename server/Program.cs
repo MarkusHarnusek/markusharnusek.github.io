9383fa1608b67949c94f69651eacef4a43c112d8
Markus Harnusek <m.harnusek@students.htl-leonding.ac.at>
2025-09-11 08:09:24 +0200

    Added basic server logic

--- server/Program.cs
+++ server/Program.cs
@@ -2,9 +2,44 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Gets the public IPv6 address and falls back to returning the public IPv4 address
+        /// </summary>
+        /// <returns></returns>
+        static async Task<string> GetPublicIpAsync()
         {
-            Console.WriteLine("Hello, World!");
+            string response = string.Empty;
+            using var httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromSeconds(3);
+
+            // Try to obtain the IPv6 address
+            try
+            {
+                response = await httpClient.GetStringAsync("https://api64.ipify.org");
+            }
+            catch (Exception IPv6e)
+            {
+                Util.Log($"Unable to obtain public IPv6 address. Falling back to IPv4: {IPv6e.Message.ToString()}", LogLevel.Warning);
+
+                // Fall back to IPv4 if unable to obtain IPv4
+                try
+                {
+                    response = await httpClient.GetStringAsync("https://api.ipify.org");
+                }
+                catch (Exception IPv4e)
+                {
+                    Util.Log($"An error occured while obtaining public IPv4 address: {IPv4e.Message.ToString()}", LogLevel.Fatal);
+                    Environment.Exit(1);
+                }
+            }
+
+            return await httpClient.GetStringAsync("https://api.ipify.org");
+        }
+
+        static async Task Main(string[] args)
+        {
+            string publicIp = await GetPublicIpAsync();
+            Util.Log($"Public IP Address: {publicIp}", LogLevel.Ok);
         }
     }
 }
